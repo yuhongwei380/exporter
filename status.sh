1aad4f87de7c9df7c179ee92c8a768a8aaa8c446
yuhongwei380 <44333391+yuhongwei380@users.noreply.github.com>
2022-05-18 16:55:08 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-sudo -s
-cd  / 
-mkdir app 
-cd /app && git clone https://github.com/yuhongwei380/exporter.git
-cd /app/expoter/
+sudo mkdir /app 
+sudo cd /app && sudo  git clone https://github.com/yuhongwei380/exporter.git
+sudo cd /app/expoter/
 sudo sed -i "s/host/$(hostname)/"  /app/exporter/filename.yml
-cd /app/exporter/ && docker-compose up -d
+cd /app/exporter/ && sudo docker-compose up -d
