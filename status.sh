967fc6d82cff143488aac4b5a770ceac48700f37
Phoenix <44333391+yuhongwei380@users.noreply.github.com>
2023-02-19 11:52:47 +0800

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo mkdir /app 
-sudo cd /app && sudo  git clone https://github.com/yuhongwei380/exporter.git
-sudo cd /app/expoter/
 sudo sed -i "s/host/$(hostname)/"  /app/exporter/filename.yml
-cd /app/exporter/ && sudo docker-compose up -d
