9c3593c7d0a0879cdfab402989071463921e17b3
Phoenix <yuhongwei380@gmail.com>
2024-09-25 11:50:04 +0800

    修正错误的部分

--- start.sh
+++ start.sh
@@ -6,4 +6,3 @@ sudo touch /var/lib/node_exporter/textfile_collector/smartmon.prom
 sudo touch /var/lib/node_exporter/textfile_collector/nvme_metrics.prom
 (sudo crontab -l; echo "*/30 * * * * /opt/exporter/smartmon.sh > /var/lib/node_exporter/textfile_collector/smartmon.prom") | sudo crontab -
 (sudo crontab -l; echo "*/30 * * * * /opt/exporter/nvme_metrics.sh > /var/lib/node_exporter/textfile_collector/nvme_metrics.prom") | sudo crontab -
-
