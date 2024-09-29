601731b2a6087389acae14cbfd5dfdbf956bbc7d
Phoenix <44333391+yuhongwei380@users.noreply.github.com>
2024-09-29 10:10:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,3 +8,4 @@ sudo chmod a+x /opt/exporter/*.sh
 #(sudo crontab -l; echo "*/30 * * * * /opt/exporter/nvme_metrics.sh > /var/lib/node_exporter/textfile_collector/nvme_metrics.prom") | sudo crontab -
 sudo touch /var/lib/node_exporter/textfile_collector/smartctl.prom
 (sudo crontab -l; echo "*/30 * * * * /opt/exporter/smartctl.sh > /var/lib/node_exporter/textfile_collector/smartctl.prom") | sudo crontab -
+bash /opt/exporter/nvme_metrics.sh > /var/lib/node_exporter/textfile_collector/nvme_metrics.prom
