6d6ef6b9d44a7a58d4bdf4ed22cf291c94088312
Phoenix <yuhongwei380@gmail.com>
2024-09-29 16:24:40 +0800

    update for OS

--- start.sh
+++ start.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-sudo apt install jq  smartmontools -y  # nvme-cli
+
+# 检测操作系统类型
+OS=$(uname -s)
+
+# 根据操作系统类型执行不同的安装命令
+if [ "$OS" = "Linux" ]; then
+    # 检测发行版
+    if [ -f /etc/debian_version ]; then
+        # Debian/Ubuntu 系列
+        sudo apt update
+        sudo apt install -y jq smartmontools
+    elif [ -f /etc/redhat-release ]; then
+        # RedHat/CentOS 系列
+        sudo yum install -y epel-release
+        sudo yum install -y jq smartmontools
+    else
+        echo "Unsupported Linux distribution"
+        exit 1
+    fi
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+
+# 创建目录和设置权限
 sudo mkdir -p /var/lib/node_exporter/textfile_collector
 sudo chmod a+x /opt/exporter/*.sh
-#sudo touch /var/lib/node_exporter/textfile_collector/smartmon.prom
-#sudo touch /var/lib/node_exporter/textfile_collector/nvme_metrics.prom
-#(sudo crontab -l; echo "*/30 * * * * /opt/exporter/smartmon.sh > /var/lib/node_exporter/textfile_collector/smartmon.prom") | sudo crontab -
-#(sudo crontab -l; echo "*/30 * * * * /opt/exporter/nvme_metrics.sh > /var/lib/node_exporter/textfile_collector/nvme_metrics.prom") | sudo crontab -
+
+# 创建 Prometheus 文件
 sudo touch /var/lib/node_exporter/textfile_collector/smartctl.prom
+
+# 设置定时任务
 (sudo crontab -l; echo "*/30 * * * * /opt/exporter/smartctl.sh > /var/lib/node_exporter/textfile_collector/smartctl.prom") | sudo crontab -
+
+# 立即运行脚本
 bash /opt/exporter/smartctl.sh > /var/lib/node_exporter/textfile_collector/smartctl.prom
