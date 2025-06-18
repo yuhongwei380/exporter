ae8159f5b4a906358ed87d33b8952b5271c74a25
Phoenix <yuhongwei380@gmail.com>
2025-06-18 17:52:29 +0800

    Update exporter.sh

--- exporter.sh
+++ exporter.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # must run as root role 
-apt install -y wget jq git 
+# judge the OS version 
+if [ -f /etc/redhat-release ]; then
+    echo "Detected CentOS/RHEL, installing wget jq git with yum..."
+    sudo yum install -y wget jq git
+elif [ -f /etc/debian_version ]; then
+    echo "Detected Ubuntu/Debian, installing wget jq git with apt..."
+    sudo apt update
+    sudo apt install -y wget jq git
+else
+    echo "Unsupported OS! Only CentOS/RHEL/Ubuntu/Debian are supported."
+    exit 1
+fi
+#install node exporter
 wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 mv node_exporter-1.9.1.linux-amd64.tar.gz  node_exporter.tar.gz
 tar -xvf node_exporter.tar.gz -C /usr/local/
@@ -25,10 +37,11 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
+systemctl daemon-reload
 systemctl enable node_exporter.service
 systemctl restart node_exporter.service
-#systemctl status node_exporter.service
-systemctl daemon-reload
 
 bash /opt/exporter/smartctl.sh > /var/lib/node_exporter/textfile_collector/smartctl.prom
 (crontab -l; echo "*/30 * * * * /opt/exporter/smartctl.sh > /var/lib/node_exporter/textfile_collector/smartctl.prom") | crontab -
+
+systemctl status node_exporter.service
